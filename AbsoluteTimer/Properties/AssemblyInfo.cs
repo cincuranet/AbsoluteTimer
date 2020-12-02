bfede333cfacf42b2012cddef4651a5c002693ab
Jiri Cincura <jiri@x2develop.com>
2020-12-02 07:34:51 +0100

    New version.

--- AbsoluteTimer/Properties/AssemblyInfo.cs
+++ AbsoluteTimer/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ED894EAD-363E-4B2A-986D-50ABE8A78141")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
